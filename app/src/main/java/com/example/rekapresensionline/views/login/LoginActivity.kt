792250be2433003febb85704dd222478a322adac
felaayulestari <felaayulestari4940@gmail.com>
2022-05-23 00:16:28 +0700

    login and forgot page

--- app/src/main/java/com/example/rekapresensionline/views/login/LoginActivity.kt
+++ app/src/main/java/com/example/rekapresensionline/views/login/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.rekapresensionline
+package com.example.rekapresensionline.views.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rekapresensionline.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
