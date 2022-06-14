d1936ad6c7df906410358cb723dcac5c51dad425
felaayulestari <felaayulestari4940@gmail.com>
2022-06-15 05:05:06 +0700

    fitur ubah password

---
+++ app/src/main/java/com/example/rekapresensionline/model/UbahPasswordResponse.kt
@@ -0,0 +1,9 @@
+package com.example.rekapresensionline.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UbahPasswordResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
